d1a7c2d9da2fb662edeb50f7703078108ad0e165
priseborough <p_riseborough@live.com.au>
2014-04-21 05:30:05 +1000

    Derivations: Updated 24 state derivation estimating terrain offset and optical flow scale factor

--- derivations/GenerateEquations24states.m
+++ derivations/GenerateEquations24states.m
@@ -5,34 +5,40 @@
 % Sequential fusion of velocity and position measurements
 % Fusion of true airspeed
 % Sequential fusion of magnetic flux measurements
-% 24 state architecture.
+% sequential fusion of angular LOS rate measurements from optical flow
+% sensor assumed to be aligned witht eh Z body axis plus a small
+% misalingment
+% 23 state architecture.
 % IMU data is assumed to arrive at a constant rate with a time step of dt
 % IMU delta angle and velocity data are used as time varying parameters,
 % not observations
 
 % Author:  Paul Riseborough
-% Last Modified: 3 Dec 2013
+% Last Modified: 20 April 2014
 
 % State vector:
 % quaternions (q0, q1, q2, q3)
 % Velocity - m/sec (North, East, Down)
 % Position - m (North, East, Down)
 % Delta Angle bias - rad (X,Y,Z)
-% Delta Velocity Bias - m/sec (X,Y,Z)
+% Delta Velocity bias - m/s (Z)
 % Wind Vector  - m/sec (North,East)
 % Earth Magnetic Field Vector - milligauss (North, East, Down)
 % Body Magnetic Field Vector - milligauss (X,Y,Z)
+% Estimated position of terrain along D axis (m) 
+% Optical flow sensor scale factor (allows for variation in optics)
 
 % Observations:
 % NED velocity - m/s
 % NED position - m
 % True airspeed - m/s
 % XYZ magnetic flux - milligauss
+% XY line of sight angular rate measurements (rel to sensor frame) from a downwards looking optical flow sensor
+% range to terrain measurement
 
 % Time varying parameters:
 % XYZ delta angle measurements in body axes - rad
 % XYZ delta velocity measurements in body axes - m/sec
-
 
 clear all;
 
@@ -43,10 +49,9 @@ syms q0 q1 q2 q3 real % quaternions defining attitude of body axes relative to l
 syms vn ve vd real % NED velocity - m/sec
 syms pn pe pd real % NED position - m
 syms dax_b day_b daz_b real % delta angle bias - rad
-syms dvx_b dvy_b dvz_b real % delta velocity bias - m/sec
+syms dvz_b real % delta velocity bias - m/sec
 syms dt real % IMU time step - sec
 syms gn ge gd real % NED gravity vector - m/sec^2
-syms omn ome omd real; % Earth rotation vector in local NED axes rad/sec - rad/sec
 syms daxCov dayCov dazCov dvxCov dvyCov dvzCov real; % IMU delta angle and delta velocity measurement variances
 syms vwn vwe real; % NE wind velocity - m/sec
 syms magX magY magZ real; % XYZ body fixed magnetic field measurements - milligauss
@@ -55,11 +60,16 @@ syms R_VN R_VE R_VD real % variances for NED velocity measurements - (m/sec)^2
 syms R_PN R_PE R_PD real % variances for NED position measurements - m^2
 syms R_TAS real  % variance for true airspeed measurement - (m/sec)^2
 syms R_MAG real  % variance for magnetic flux measurements - milligauss^2
-
+syms R_LOS real % variance of LOS angular rate mesurements (rad/sec)^2
+syms R_RNG real % variance of laser range finder observations
+syms ptd real % location of terrain in D axis
+syms a1 a2 a3 real % misalignment of the optical flow sensor (rad)
+syms a4 real % pitch alignment of the laser range finder (rad)
+syms Kopt real % optical flow sensor LOS rate scale factor
 %% define the process equations
 
 % Define the state vector & number of states
-stateVector = [q0;q1;q2;q3;vn;ve;vd;pn;pe;pd;dax_b;day_b;daz_b;dvx_b;dvy_b;dvz_b;vwn;vwe;magN;magE;magD;magX;magY;magZ];
+stateVector = [q0;q1;q2;q3;vn;ve;vd;pn;pe;pd;dax_b;day_b;daz_b;dvz_b;vwn;vwe;magN;magE;magD;magX;magY;magZ;ptd;Kopt];
 nStates=numel(stateVector);
 
 % define the measured Delta angle and delta velocity vectors
@@ -68,46 +78,35 @@ dv = [dvx; dvy; dvz];
 
 % define the delta angle and delta velocity bias errors
 da_b = [dax_b; day_b; daz_b];
-dv_b = [dvx_b; dvy_b; dvz_b];
+dv_b = [0; 0; dvz_b];
 
 % derive the body to nav direction cosine matrix
 Tbn = Quat2Tbn([q0,q1,q2,q3]);
 
-% define the bias corrected delta angle
-% Ignore coning compensation and earths rotation as these effect are
-% negligible in terms of covariance growth compared to other efects for our
-% grade of sensor
-% deltaAngle = da - da_b + 1/12*cross(da_prev,da) - transpose(Cbn)*([omn; ome; omd])*dt;
-deltaAngle = da - da_b;
-
-% define the bias corrected delta velocity
-% Ignore sculling as this effect is negligible in terms of covariance growth 
-% compared to other effects for our grade of sensor
-% deltaVelocity = dv - dv_b + 0.5*cross(da,dv) + 1/12*(cross(da_prev,dv) + cross(dv_prev,da));
-deltaVelocity = dv - dv_b;
+% define the bias corrected delta angles and velocities
+dAngCor = da - da_b;
+dVelCor = dv - dv_b;
 
 % define the quaternion rotation vector
 quat = [q0;q1;q2;q3];
 
 % define the attitude update equations
-% use a first order expansion of rotation to calculate the quaternion increment
-% acceptable for propagation of covariances
 delQuat = [1;
-    0.5*deltaAngle(1);
-    0.5*deltaAngle(2);
-    0.5*deltaAngle(3);
+    0.5*dAngCor(1);
+    0.5*dAngCor(2);
+    0.5*dAngCor(3);
     ];
 qNew = QuatMult(quat,delQuat);
 
 % define the velocity update equations
-vNew = [vn;ve;vd] + [gn;ge;gd]*dt + Tbn*deltaVelocity;% - cross(2*[omn; ome; omd],[vn;ve;vd])*dt;
+vNew = [vn;ve;vd] + [gn;ge;gd]*dt + Tbn*dVelCor;
 
 % define the position update equations
 pNew = [pn;pe;pd] + [vn;ve;vd]*dt;
 
 % define the IMU bias error update equations
 dabNew = [dax_b; day_b; daz_b];
-dvbNew = [dvx_b; dvy_b; dvz_b];
+dvbNew = dvz_b;
 
 % define the wind velocity update equations
 vwnNew = vwn;
@@ -123,8 +122,14 @@ magXnew = magX;
 magYnew = magY;
 magZnew = magZ;
 
+% define the terrain position update equation
+ptdNew = ptd;
+
+% define the optical flow scale factor update equation
+KoptNew = Kopt;
+
 % Define the process equations output vector
-processEqns = [qNew;vNew;pNew;dabNew;dvbNew;vwnNew;vweNew;magNnew;magEnew;magDnew;magXnew;magYnew;magZnew];
+processEqns = [qNew;vNew;pNew;dabNew;dvbNew;vwnNew;vweNew;magNnew;magEnew;magDnew;magXnew;magYnew;magZnew;ptdNew;KoptNew];
 
 %% derive the state transition matrix
 
@@ -140,6 +145,10 @@ F = jacobian(processEqns, stateVector);
 % solution is assumed to be driven by 'noise' in the delta angles and
 % velocities, after bias effects have been removed. This is OK becasue we
 % have sensor bias accounted for in the state equations.
+
+% Error growth in other states is not defined here and is handled as part
+% of the implementation using a simple error growth model with process
+% noise variances defined separately for each state
 distVector = [da;dv];
 
 % derive the control(disturbance) influence matrix
@@ -147,23 +156,19 @@ G = jacobian(processEqns, distVector);
 [G,SG]=OptimiseAlgebra(G,'SG');
 
 % derive the state error matrix
-% Note - this derivation of the covariance update equations does not include
-% any general state noise - this will be added at the implementation stage
-% by adding an approriately scaled diagonal process noise matrix to the 
-% covariance matrix
 imuNoise = diag([daxCov dayCov dazCov dvxCov dvyCov dvzCov]);
 Q = G*imuNoise*transpose(G);
 [Q,SQ]=OptimiseAlgebra(Q,'SQ');
 
 % define a symbolic covariance matrix using strings to represent 
-% '_lp_' to represent '( '
+% '_l_' to represent '( '
 % '_c_' to represent ,
-% '_rp_' to represent ')' 
+% '_r_' to represent ')' 
 % these can be substituted later to create executable code
 for rowIndex = 1:nStates
     for colIndex = 1:nStates
-        eval(['syms OP_lp_',num2str(rowIndex),'_c_',num2str(colIndex), '_rp_ real']);
-        eval(['P(',num2str(rowIndex),',',num2str(colIndex), ') = OP_lp_',num2str(rowIndex),'_c_',num2str(colIndex),'_rp_;']);
+        eval(['syms OP_l_',num2str(rowIndex),'_c_',num2str(colIndex), '_r_ real']);
+        eval(['P(',num2str(rowIndex),',',num2str(colIndex), ') = OP_l_',num2str(rowIndex),'_c_',num2str(colIndex),'_r_;']);
     end
 end
 
@@ -193,7 +198,7 @@ H_PD= jacobian(pd,stateVector); % measurement Jacobian
 K_PD = (P*transpose(H_PD))/(H_PD*P*transpose(H_PD) + R_PD);
 
 % combine into a single H and K matrix (note these matrices cannot be used
-% for a single step fusion, so each row|column mst be used in a separate
+% for a single step fusion, so each row|column must be used in a separate
 % fusion step
 H_VP  = [H_VN;H_VE;H_VD;H_PN;H_PE;H_PD];
 clear    H_VN H_VE H_VD H_PN H_PE H_PD
@@ -218,6 +223,58 @@ K_MY = (P*transpose(H_MAG(2,:)))/(H_MAG(2,:)*P*transpose(H_MAG(2,:)) + R_MAG); %
 [K_MY,SK_MY]=OptimiseAlgebra(K_MY,'SK_MY');
 K_MZ = (P*transpose(H_MAG(3,:)))/(H_MAG(3,:)*P*transpose(H_MAG(3,:)) + R_MAG); % Kalman gain vector
 [K_MZ,SK_MZ]=OptimiseAlgebra(K_MZ,'SK_MZ');
+
+%% derive equations for sequential fusion of optical flow measurements
+
+% assume senosr axes are aligned with body axis plus a misalignment
+% defined by 3 small angles about X, Y and Z axis
+% Tsb is rotation from sensor to body
+Tsb = [ 1  , -a3  ,  a2 ; ...
+        a3 ,  1   , -a1 ; ...
+       -a2 ,  a1  ,  1 ];
+Tsn = Tbn*Tsb; 
+% calculate range from plane to centre of sensor fov assuming flat earth
+range = ((ptd - pd)/Tsn(3,3));
+% calculate relative velocity in sensor frame
+relVelSensor = transpose(Tsn)*[vn;ve;vd];
+% calculate angular rates in sensor frame
+omegaSensor = transpose(Tsb)*dAngCor/dt;
+% divide by range to get predicted angular LOS rates relative to X and Y
+% sensor axes and apply scale factor
+losRateX =  Kopt*(relVelSensor(2)/range - omegaSensor(1));
+losRateY = Kopt*(-relVelSensor(1)/range - omegaSensor(2));
+
+H_LOS = jacobian([losRateX;losRateY],stateVector); % measurement Jacobian
+[H_LOS,SH_LOS]=OptimiseAlgebra(H_LOS,'SH_LOS');
+
+% combine into a single K matrix to enable common expressions to be found
+% note this matrix cannot be used in a single step fusion
+K_LOSX = (P*transpose(H_LOS(1,:)))/(H_LOS(1,:)*P*transpose(H_LOS(1,:)) + R_LOS); % Kalman gain vector
+K_LOSY = (P*transpose(H_LOS(2,:)))/(H_LOS(2,:)*P*transpose(H_LOS(2,:)) + R_LOS); % Kalman gain vector
+K_LOS = [K_LOSX,K_LOSY];
+[K_LOS,SK_LOS]=OptimiseAlgebra(K_LOS,'SK_LOS');
+[SK_LOS,SKK_LOS]=OptimiseAlgebra(SK_LOS,'SKK_LOS');
+
+%% derive equations for fusion of laser range finder measurement
+
+% assume laser is aligned in the X-Z body plane plus an alignment
+% defined by rotation about the Y body axis. This allows the sensor to be
+% aligned to accomodate different aircraft pitch trims. We would normally
+% want the sensor to be aligned so that it was close to vertical during the
+% phases of flight where height accuracy is most important
+% Tsb is rotation from sensor to body
+Tsb = [ 1       ,  0 , sin(a4) ; ...
+        0       ,  1 , 0       ; ...
+       -sin(a4) ,  0 , 1      ];
+Tsn = Tbn*Tsb; 
+% calculate range from plane to centre of sensor fov assuming flat earth
+range = ((ptd - pd)/Tsn(3,3));
+H_RNG = jacobian(range,stateVector); % measurement Jacobian
+[H_RNG,SH_RNG]=OptimiseAlgebra(H_RNG,'SH_RNG');
+
+% calculate the Kalman gain matrix and optimise algebra
+K_RNG = (P*transpose(H_RNG))/(H_RNG*P*transpose(H_RNG) + R_RNG);
+[K_RNG,SK_RNG]=OptimiseAlgebra(K_RNG,'SK_RNG');
 
 %% Save output and convert to m and c code fragments
 nStates = length(PP);
