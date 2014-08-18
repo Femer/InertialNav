3f27b6ea57790f8f145b692e4ce69b34a9b52f67
Lorenz Meier <lm@inf.ethz.ch>
2014-08-18 12:08:32 +0200

    Allow to plot wind including lowpass

--- code/plot_wind.py
+++ code/plot_wind.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -17,18 +17,22 @@ import math
 	# 19-21: Body Magnetic Field Vector - milligauss (X,Y,Z)
 	# 22: Terrain
 try:
-	data = np.genfromtxt('StateDataOut.txt', delimiter=' ', skip_header=1,
-		skip_footer=1, names=['time', 'q1', 'q2', 'q3', 'q4', 'Vn', 'Ve', 'Vd', 'Pn', 'Pe', 'Pd',
-		'Bx', 'By', 'Bz', 'Aoff', 'Wn', 'We', 'Mn', 'Me', 'Md', 'Mbn', 'Mbe', 'Mbd', 'dist'])
-except ValueError:
+	data = np.genfromtxt('WindFuse.txt', delimiter=' ', skip_header=1,
+		skip_footer=1, names=['time', 'Wn', 'We', 'Wfn', 'Wfe'])
+except:
 	try:
 		data = np.genfromtxt('StateDataOut.txt', delimiter=' ', skip_header=1,
-		skip_footer=1, names=['time', 'q1', 'q2', 'q3', 'q4', 'Vn', 'Ve', 'Vd', 'Pn', 'Pe', 'Pd',
-		'Bx', 'By', 'Bz', 'Aoff', 'Wn', 'We', 'Mn', 'Me', 'Md', 'Mbn', 'Mbe', 'Mbd'])
-	except ValueError:
-		data = np.genfromtxt('StateDataOut.txt', delimiter=' ', skip_header=1,
 			skip_footer=1, names=['time', 'q1', 'q2', 'q3', 'q4', 'Vn', 'Ve', 'Vd', 'Pn', 'Pe', 'Pd',
-			'Bx', 'By', 'Bz', 'Wn', 'We', 'Mn', 'Me', 'Md', 'Mbn', 'Mbe', 'Mbd'])
+			'Bx', 'By', 'Bz', 'Aoff', 'Wn', 'We', 'Mn', 'Me', 'Md', 'Mbn', 'Mbe', 'Mbd', 'dist'])
+	except ValueError:
+		try:
+			data = np.genfromtxt('StateDataOut.txt', delimiter=' ', skip_header=1,
+			skip_footer=1, names=['time', 'q1', 'q2', 'q3', 'q4', 'Vn', 'Ve', 'Vd', 'Pn', 'Pe', 'Pd',
+			'Bx', 'By', 'Bz', 'Aoff', 'Wn', 'We', 'Mn', 'Me', 'Md', 'Mbn', 'Mbe', 'Mbd'])
+		except ValueError:
+			data = np.genfromtxt('StateDataOut.txt', delimiter=' ', skip_header=1,
+				skip_footer=1, names=['time', 'q1', 'q2', 'q3', 'q4', 'Vn', 'Ve', 'Vd', 'Pn', 'Pe', 'Pd',
+				'Bx', 'By', 'Bz', 'Wn', 'We', 'Mn', 'Me', 'Md', 'Mbn', 'Mbe', 'Mbd'])
 
 fig = plt.figure()
 
@@ -38,11 +42,20 @@ ax1.set_title("Wind Velocity")
 ax1.set_xlabel('time (s)')
 ax1.set_ylabel('Wind North')
 ax1.plot(data['time'], data['Wn'], color='r', label='Wind N')
+try:
+	ax1.plot(data['time'], data['Wfn'], color='b', label='Wind N filtered')
+except:
+	print "No filtered north wind data"
+
 
 ax2 = fig.add_subplot(212)
   
 ax2.set_xlabel('time (s)')
 ax2.set_ylabel('Wind East')
 ax2.plot(data['time'], data['We'], color='g', label='Wind E')
+try:
+	ax2.plot(data['time'], data['Wfe'], color='m', label='Wind E filtered')
+except:
+	print "No filtered east wind data"
 
 plt.show()
